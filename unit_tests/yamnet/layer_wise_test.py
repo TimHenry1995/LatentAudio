895e84c6ae35069475e4b94fbd6cabad9fd538d3
TimHenry1995 <52284098+TimHenry1995@users.noreply.github.com>
2023-08-17 11:53:59 +0200

    path management

--- unit_tests/yamnet/layer_wise_test.py
+++ unit_tests/yamnet/layer_wise_test.py
@@ -2,7 +2,7 @@ import unittest
 import latent_audio.yamnet.layer_wise as ylw
 import latent_audio.plugins.yamnet.yamnet as yamnet_lib
 import latent_audio.plugins.yamnet.params as params_lib
-import tensorflow as tf, numpy as np
+import tensorflow as tf, numpy as np, os
 
 class TestLayerWiseYamnet(unittest.TestCase):
     """Collects unit tests for the class LayerWiseYamnet"""
@@ -12,11 +12,11 @@ class TestLayerWiseYamnet(unittest.TestCase):
 
         # Initialize
         layer_wise_yamnet = ylw.LayerWiseYamnet()
-        layer_wise_yamnet.load_weights(file_path='src/latent_audio/plugins/yamnet/yamnet.h5')
+        layer_wise_yamnet.load_weights(file_path=os.path.join('src','latent_audio','plugins','yamnet','yamnet.h5'))
 
         waveform = np.sin(1000*np.pi*np.linspace(0,10,100000), dtype=np.float32)
         yamnet = yamnet_lib.yamnet_frames_model(params_lib.Params())
-        yamnet.load_weights('src/latent_audio/plugins/yamnet/yamnet.h5')
+        yamnet.load_weights(os.path.join('src','latent_audio','plugins','yamnet','yamnet.h5'))
         
         for layer_index in range(14):
             probabilities_1 = layer_wise_yamnet.call_from_layer(layer_wise_yamnet.call_until_layer(waveform, layer_index=layer_index),layer_index=layer_index) 
