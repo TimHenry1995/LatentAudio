d668bd75fc1238700845c9b1638d9838f476e93c
TimHenry1995 <52284098+TimHenry1995@users.noreply.github.com>
2023-08-23 16:52:27 +0200

    added cross validation

--- src/latent_audio/scripts/explore_pre_processed.py
+++ src/latent_audio/scripts/explore_pre_processed.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import cross_val_score
 # Configuration
 factor_name = 'Material' # or alternatively 'Action'
 data_folder = os.path.join('data','pre-processed')
-sample_size = 1012 # The number of instances to be visualized
+sample_size = 128 # The number of instances to be visualized
 np.random.seed(42)
 label_to_material = ['W','M','G','S','C','P']
 label_to_action = ['T','R','D','W']
@@ -25,8 +25,6 @@ else:
     label_to_factor = label_to_action
     factor_index = 1
 
-np.random.seed(42) # Reproducability
-
 # Load layer indices
 layer_indices = []
 for folder_name in os.listdir(data_folder):
@@ -34,11 +32,12 @@ for folder_name in os.listdir(data_folder):
 layer_indices.sort()
 layer_indices = [0,6,13]
 # For each layer
-explained_variances = [None] * len(layer_indices)
-projections = [None] * len(layer_indices)
-Ys = [None] * len(layer_indices)
-KNN_scores = [None] * len(layer_indices)
-for l, layer_index in enumerate(layer_indices):
+explained_variances = {}
+projections = {}
+KNN_accuracies = {}
+Ys = {}
+
+for layer_index in layer_indices:
     # Convenience variables
     layer_name = f"Layer {layer_index}"
 
@@ -55,21 +54,21 @@ for l, layer_index in enumerate(layer_indices):
         x_path = os.path.join(data_folder, layer_name, str(x_file_names[j]))
         X[i] = np.load(x_path)[np.newaxis,:]; Y[i] = np.load(x_path.replace('_X_','_Y_'))[np.newaxis,:]
     X = np.concatenate(X, axis=0); Y = np.concatenate(Y, axis=0)
-    Ys[l] = Y
+    Ys[layer_index] = Y
 
     # Fit PCA 
     pca = PCA(n_components=50)
     pca.fit(X)
-    explained_variances[l] = pca.explained_variance_ratio_
+    explained_variances[layer_index] = pca.explained_variance_ratio_
     X = pca.transform(X)
     
     # Fit KNN
     KNN = KNeighborsClassifier(n_neighbors=3)
-    KNN_scores[l] = cross_val_score(KNN, X, Y[:,factor_index], cv=5)
+    KNN_accuracies[layer_index] = cross_val_score(KNN, X, Y[:,factor_index], cv=5)
     
     # Fit t-SNE
     tsne = TSNE()
-    projections[l] = tsne.fit_transform(X)
+    projections[layer_index] = tsne.fit_transform(X)
     
 # Plot t-SNE
 plt.figure(figsize=(10,3))
@@ -79,10 +78,10 @@ for l, layer_index in {layer_indices.index(element): element for element in [0,6
     
     plt.subplot(1,3,p+1); p+= 1
     # Iterate classes of materials
-    factor_labels = set(list(np.reshape(Ys[l][:,1], [-1])))
+    factor_labels = set(list(np.reshape(Ys[layer_index][:,1], [-1])))
     for label in factor_labels:
-        label_indices = Ys[l][:, factor_index] == label
-        plt.scatter(projections[l][label_indices,0], projections[l][label_indices,1], marker='.')
+        label_indices = Ys[layer_index][:, factor_index] == label
+        plt.scatter(projections[layer_index][label_indices,0], projections[layer_index][label_indices,1], marker='.')
         plt.xlabel(f'Dimension 1\nLayer {layer_index}')
     if l == 0: 
         plt.legend([label_to_factor[label] for label in factor_labels])
@@ -90,19 +89,17 @@ for l, layer_index in {layer_indices.index(element): element for element in [0,6
     plt.xticks([]); plt.yticks([])
 
 # Plot KNN
-accuracy_means = KNN_scores
 plt.figure(figsize=(10,5)); plt.title(f"K-Nearest Neighbor Classification on {factor_name}s")
-plt.bar([str(i) for i in layer_indices], accuracies)
+plt.boxplot(KNN_accuracies.values(), labels=KNN_accuracies.keys())
 plt.ylim(0,1); plt.ylabel('Accuray'); plt.xlabel('Layer')
-
 
 # Plot PCA
 plt.figure(figsize=(10,5)); plt.title("Principal Component Analysis")
-for l, layer_index in enumerate(layer_indices):
+for layer_index in layer_indices:
     R = 0
     plt.gca().set_prop_cycle(None)
-    for i, r in enumerate(explained_variances[l]):
-        plt.bar([str(layer_index)],[r], bottom=R)
+    for i, r in enumerate(explained_variances[layer_index]):
+        plt.bar([str(layer_index)],[r], bottom=R, color='white', edgecolor='black')
         R += r
     
     plt.ylim(0,1)
