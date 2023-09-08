25c7b99573ffde299f159b2a4a528fed91ea2157
TimHenry1995 <52284098+TimHenry1995@users.noreply.github.com>
2023-09-08 12:20:35 +0200

    Lots of advancements

--- src/latent_audio/scripts/disentangle.py
+++ src/latent_audio/scripts/disentangle.py
@@ -1,47 +1,26 @@
 """This script creates a flow model and calibrates it to disentangle latent factors of layerwise yamnet. It assumes that the data
 is preprocessed by pre_process.py."""
 
-from typing import List, Any, OrderedDict, Callable
+from typing import List, Any, OrderedDict, Callable, Generator, Tuple
 from gyoza.modelling import data_iterators as gmd, flow_layers as mfl, standard_layers as msl, masks as gmm
 import os, numpy as np
 import tensorflow as tf, matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-import warnings
-# Configuration
-inspection_layer_index = 8
-data_path = os.path.join('data','pre-processed','64 PCA dimensions all in 1 file',f"Layer {inspection_layer_index}")
-batch_size = 1024
-np.random.seed(42) # Answer to everything
-stage_count = 5
-epoch_count = 10
-batch_count = 10
-model_save_path = os.path.join('models', f'{stage_count} stages {epoch_count} epochs {batch_count} batches.h5')
-plot_save_path = os.path.join('plots','disentangle',f'{stage_count} stages {epoch_count} epochs {batch_count} batches')
-## Create data iterator
+import warnings, random
+from sklearn.decomposition import PCA
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
 
-# Create the iterator
-def similarity_function(Y_a: np.ndarray, Y_b: np.ndarray) -> np.ndarray:
-    Y_ab = Y_a == Y_b
-    residual_factor_zeros = np.zeros([Y_ab.shape[0],1], dtype=Y_ab.dtype) # Shape == [instance count, 1]. For simplicity, instances are assumed to be residually different
-    Y_ab = np.concatenate([residual_factor_zeros, Y_ab], axis=1)
-    return Y_ab
-
-Z = np.load(os.path.join(data_path,'X.npy')); Y=np.load(os.path.join(data_path,'Y.npy'))
-Z_train, Z_test, Y_train, Y_test = train_test_split(Z,Y, test_size=0.33, random_state=42)
-iterator = gmd.volatile_factorized_pair_iterator(X=Z_train, Y=Y_train, similarity_function=similarity_function, batch_size=batch_size)
-Z_ab_sample, Y_ab_sample = next(iterator) # Sample
-print("The data is fed to the model in batches of shape:\n","Z: (instance count, pair, dimensionality): \t", Z_ab_sample.shape,'\nY_sample: (instance count, factor count): \t', Y_ab_sample.shape)
-
-# Create a flow model
+# Define some functions
 def create_network(Z_sample: np.ndarray, stage_count: int, dimensions_per_factor: List[int]) -> mfl.SupervisedFactorNetwork:
-    """This function creates a supervised factor network that has ``stage_count`` many consecutive processing stages. Each stage 
+    """This function creates a supervised factor network that has ``stage_count`` many consecutive processing stages. Each stage
     consists of a reflection layer with 8 reflections, followed by a full coupling layer (two additive coupling layers, each
-    followed by a SquareWave permutation, the latter with a nagative mask) and an activation normalization layer. During inference, 
-    the network takes input :math:`Z` and outputs :math:'\tilde{Z}' which each have shape [:math:`M`, :math:`N`], where :math:`M` is 
+    followed by a SquareWave permutation, the latter with a nagative mask) and an activation normalization layer. During inference,
+    the network takes input :math:`Z` and outputs :math:'\tilde{Z}' which each have shape [:math:`M`, :math:`N`], where :math:`M` is
     the instance count and :math:`N` is the dimensionality.
 
-    :param Z_sample: A representative sample of the data to be fed through the model. This is needed to initialize some layer 
-        parameters. Shape is assumed to be ["math"`M`, :math:`N`], where :math:`M` is the sample size and :math:`N` is the 
+    :param Z_sample: A representative sample of the data to be fed through the model. This is needed to initialize some layer
+        parameters. Shape is assumed to be ["math"`M`, :math:`N`], where :math:`M` is the sample size and :math:`N` is the
         dimensionality of an instance.
     :type Z_sample: :class:`numpy.ndarray`
     :param stage_count: The number of processing stages that the ``network`` shall use.
@@ -55,104 +34,109 @@ def create_network(Z_sample: np.ndarray, stage_count: int, dimensions_per_factor
 
     # Set up the coupling functions and masks for the coupling layers
     dimensionality = Z_sample.shape[-1]
-    layers = [None] * (1+6*stage_count)
+    layers = [None] * (1+7*stage_count)
     layers[0] = mfl.ActivationNormalization(axes=[1], shape=[dimensionality])
     for i in range(stage_count):
-        layers[6*i+1] = mfl.Reflection(axes=[1], shape=[dimensionality], reflection_count=8)
+        layers[7*i+1] = mfl.Shuffle(axes=[1], shape=[dimensionality])
 
         compute_coupling_parameters_1 = msl.BasicFullyConnectedNet(latent_dimension_count=dimensionality, output_dimension_count=dimensionality, depth=3)
         mask_1 = gmm.CheckerBoard(axes=[1], shape=[dimensionality])
 
-        layers[6*i+2] = mfl.AdditiveCoupling(axes=[1], shape=[dimensionality], compute_coupling_parameters=compute_coupling_parameters_1, mask=mask_1)
-        layers[6*i+3] = mfl.CheckerBoard(axes=[1], shape=[dimensionality])
+        layers[7*i+2] = mfl.AdditiveCoupling(axes=[1], shape=[dimensionality], compute_coupling_parameters=compute_coupling_parameters_1, mask=mask_1)
+        layers[7*i+3] = mfl.CheckerBoard(axes=[1], shape=[dimensionality])
 
         compute_coupling_parameters_2 = msl.BasicFullyConnectedNet(latent_dimension_count=dimensionality, output_dimension_count=dimensionality, depth=3)
         mask_2 = gmm.CheckerBoard(axes=[1], shape=[dimensionality])
 
-        layers[6*i+4] = mfl.AdditiveCoupling(axes=[1], shape=[dimensionality], compute_coupling_parameters=compute_coupling_parameters_2, mask=mask_2)
-        layers[6*i+5] = mfl.CheckerBoard(axes=[1], shape=[dimensionality])
+        layers[7*i+4] = mfl.AdditiveCoupling(axes=[1], shape=[dimensionality], compute_coupling_parameters=compute_coupling_parameters_2, mask=mask_2)
+        layers[7*i+5] = mfl.CheckerBoard(axes=[1], shape=[dimensionality])
 
-        layers[6*i+6] = mfl.ActivationNormalization(axes=[1], shape=[dimensionality])
+        layers[7*i+6] = mfl.ActivationNormalization(axes=[1], shape=[dimensionality])
+
+        layers[7*i+7] = mfl.Reflection(axes=[1], shape=[dimensionality], reflection_count=8)
 
     # Construct the network
-    network = mfl.SupervisedFactorNetwork(sequence=layers, dimensions_per_factor=dimensions_per_factor) 
+    network = mfl.SupervisedFactorNetwork(sequence=layers, dimensions_per_factor=dimensions_per_factor, sigma=0.999)
     network(Z_sample) # Initialization of some layer parameters
 
     # Outputs
     return network
 
-# Take a few batches, to ensure enough similar pairs are found for dimensionality estimation
-'''sample_count = 10
-Z_ab_sample_large = [None] * sample_count; Y_ab_sample_large = [None] * sample_count
-for i in range(sample_count):
-    Z_ab_sample_large[i], Y_ab_sample_large[i] = next(iterator)
-Z_ab_sample_large = np.concatenate(Z_ab_sample_large, axis=0)
-Y_ab_sample_large = np.concatenate(Y_ab_sample_large, axis=0)
+def load_iterators(data_path: str, materials_to_drop: List[int], actions_to_drop: List[int], batch_size: int) -> Tuple[Generator, Generator]:
 
-# Estimate dimensionality per factor
-dimensions_per_factor = mfl.SupervisedFactorNetwork.estimate_factor_dimensionalities(Z_ab=Z_ab_sample_large, Y_ab=Y_ab_sample_large)
-'''
-dimensions_per_factor = [32,16,16]
-flow_network = create_network(Z_sample=Z_ab_sample[:,0,:], stage_count=stage_count, dimensions_per_factor=dimensions_per_factor)
+    # Load the data
+    Z = np.load(os.path.join(data_path,'X.npy')); Y=np.load(os.path.join(data_path,'Y.npy'))
 
-# Calibrate
-flow_network.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.005), loss=tf.keras.losses.MeanSquaredError())
-epoch_loss_means, epoch_loss_standard_deviations = flow_network.fit(epoch_count=epoch_count, batch_count=batch_count, iterator=iterator)
-plt.figure(figsize=(10,3)); plt.title('Loss Trajectory'); plt.plot(epoch_loss_means); plt.xlabel('Epoch'); plt.ylabel('Loss')
-plt.savefig(plot_save_path + ' Loss.png'); plt.show()
+    # Eliminate classes to drop
+    for m in materials_to_drop:
+        Z = Z[Y[:,0] != m,:]; Y = Y[Y[:,0] != m,:]
+    for a in actions_to_drop:
+        Z = Z[Y[:,1] != a,:]; Y = Y[Y[:,1] != a,:]
 
-# Save existing model
-path = "example_model.h5"
-flow_network.save_weights(model_save_path)
-#new_model = create_network(Z_sample=Z_ab_sample[:,0,:], stage_count=stage_count, dimensions_per_factor=dimensions_per_factor)
+    # standardize
+    Z=(Z-np.mean(Z,axis=0))/np.std(Z,axis=0) 
 
-# Load weights
-#new_model.load_weights(model_save_path); 
+    # Train test split
+    Z_train, Z_test, Y_train, Y_test = train_test_split(Z,Y, test_size=0.33, random_state=53)
 
-# Compare
-#print("The saved and the loaded model weights produce a prediction difference equal to", tf.reduce_sum((flow_network(Z_ab_sample[:,0,:])-new_model(Z_ab_sample[:,0,:]))**2))
-#del new_model
+    # Fit PCA
+    pca = PCA(n_components=16)
+    pca.fit(Z_test)
+    Z_test = pca.transform(Z_test); Z_train = pca.transform(Z_train)
 
-def evaluate_factor_sensitivity(flow_network: mfl.SupervisedFactorNetwork, iterator: Callable, sample_size: int, dimensions_per_factor: List[int]) -> None:
+    # Create the iterators
+    def similarity_function(Y_a: np.ndarray, Y_b: np.ndarray) -> np.ndarray:
+        Y_ab = Y_a == Y_b
+        residual_factor_zeros = np.zeros([Y_ab.shape[0],1], dtype=Y_ab.dtype) # Shape == [instance count, 1]. For simplicity, instances are assumed to be residually different
+        Y_ab = np.concatenate([residual_factor_zeros, Y_ab], axis=1)
+        return Y_ab
 
-    # Compute distance between pairs 
-    delta_ma = compute_paired_differences(flow_network=flow_network, iterator=iterator, equal_material=True, equal_action=True, sample_size=sample_size) # ma means equal material, equal action
-    delta_m = compute_paired_differences(flow_network=flow_network, iterator=iterator, equal_material=True, equal_action=False, sample_size=sample_size) # m means equal material, different action
-    delta_a = compute_paired_differences(flow_network=flow_network, iterator=iterator, equal_material=False, equal_action=True, sample_size=sample_size) # a means different material, equal action
+    train_iterator = gmd.volatile_factorized_pair_iterator(X=Z_train, Y=Y_train, similarity_function=similarity_function, batch_size=batch_size)
+    test_iterator = gmd.volatile_factorized_pair_iterator(X=Z_test, Y=Y_test, similarity_function=similarity_function, batch_size=batch_size)
+    batch_count = len(Z_train)//batch_size
 
-    count = 0
-    distance = lambda x: tf.sqrt(tf.reduce_sum(x**2, axis=1)) # Shape of output == [instance count]
-    factor_names = ['Residual','Material','Action']
-    for f, dimension_count in enumerate(dimensions_per_factor):
-        # Plot
-        plt.figure(); plt.title(f'Sensitivity Of {factor_names[f]} Dimensions '+ r'Of $\tilde{Z}$' + '\nTo Changes In Z')
-        plt.violinplot([distance(delta_ma[:,count:count+dimension_count]), distance(delta_m[:,count:count+dimension_count]), distance(delta_a[:,count:count+dimension_count])], showmeans=True)
-        plt.xticks(ticks=[1,2,3], labels=['Same Material\nSame Action', 'Same Material\nOther Action', 'Other Material\nSame Action'])
-        plt.ylabel('Distance'); plt.xlabel("Input Pair")
-        count += dimension_count
-        plt.savefig(plot_save_path + f' Sensitivity {factor_names[f]}.png')
-    plt.show()
+    return train_iterator, test_iterator, batch_count, Z_train, Z_test, Y_train, Y_test
 
-def compute_paired_differences(flow_network: mfl.SupervisedFactorNetwork, iterator: Callable, equal_material: bool, equal_action: bool, sample_size: int) -> None:
+if __name__ == "__main__":
 
-    # Initialization
-    Z_ab = []
-    current_sample_size = 0
+    # Configuration
+    inspection_layer_index = 8
+    data_path = os.path.join('data','pre-processed','16 PCA dimensions all in 1 file','Layer 8')
+    batch_size = 512
+    np.random.seed(850)
+    tf.keras.utils.set_random_seed(125)
+    random.seed(946)
+    stage_count = 10
+    epoch_count = 50
+    dimensions_per_factor = [14,1,1]
+    materials_to_keep = [1,4]; actions_to_keep = [0,1]
+    materials_to_drop = list(range(6))
+    for m in reversed(materials_to_keep): materials_to_drop.remove(m)
+    actions_to_drop = list(range(4))
+    for a in reversed(actions_to_keep): actions_to_drop.remove(a)
+    m_string = ",".join(str(m) for m in materials_to_keep)
+    a_string = ",".join(str(a) for a in actions_to_keep)
+    model_save_path = os.path.join('models', f'Layer {inspection_layer_index}')
+    plot_save_path = os.path.join('plots','disentangle', f'Layer {inspection_layer_index}')
+    if not os.path.exists(model_save_path): os.makedirs(model_save_path)
+    if not os.path.exists(plot_save_path): os.makedirs(plot_save_path)
+
+    # Load data iterators
+    train_iterator, test_iterator, batch_count,_,_,_,_ = load_iterators(data_path=data_path, materials_to_drop=materials_to_drop, actions_to_drop=actions_to_drop, batch_size=batch_size)
+    Z_ab_sample, Y_ab_sample = next(train_iterator) # Sample
     
-    # Accumulate instances to meet the sample size
-    while current_sample_size < sample_size:
-        Z_ab_batch, Y_ab_batch = next(iterator)
-        Z_ab.append(Z_ab_batch.numpy()[np.logical_and(Y_ab_batch[:,1] == equal_material, Y_ab_batch[:,2] == equal_action),:])
-        current_sample_size += Z_ab[-1].shape[0]
+    print("The data is fed to the model in batches of shape:\n","Z: (instance count, pair, dimensionality): \t", Z_ab_sample.shape,'\nY_sample: (instance count, factor count): \t', Y_ab_sample.shape)
 
-    # Obtain proper shape
-    Z_ab = np.concatenate(Z_ab, axis=0) # Instance axis
-    Z_ab = Z_ab[:sample_size,:] # Shape == [sample_size, 2, ...] where 2 is for pair and ... is for a single instance 
-    
-    # Distance vectors in disentangled space
-    delta = tf.reshape(flow_network(Z_ab[:,0,:]) - flow_network(Z_ab[:,1,:]), [sample_size, -1]) # Shape == [instance count, dimension count]
-    
-    # Output
-    return delta
+    # Create network
+    flow_network = create_network(Z_sample=Z_ab_sample[:,0,:], stage_count=stage_count, dimensions_per_factor=dimensions_per_factor)
 
-evaluate_factor_sensitivity(flow_network=flow_network, iterator=iterator, sample_size=100, dimensions_per_factor=dimensions_per_factor)
+    # Calibrate
+    flow_network.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001))
+    epoch_loss_means, epoch_loss_standard_deviations, epoch_loss_means_validate, epoch_loss_standard_deviations_validate = flow_network.fit(epoch_count=epoch_count, batch_count=batch_count, iterator=train_iterator, iterator_validate=test_iterator)
+    plt.figure(figsize=(10,3)); plt.title('Loss Trajectory');
+    plt.plot(epoch_loss_means); plt.plot(epoch_loss_means_validate);
+    plt.xlabel('Epoch'); plt.ylabel('Loss')
+    plt.savefig(os.path.join(plot_save_path, f'materials {m_string} actions {a_string} stages {stage_count} epochs {epoch_count} Loss.png')); plt.show()
+
+    # Save existing model
+    flow_network.save_weights(os.path.join(model_save_path, f'materials {m_string} actions {a_string} stages {stage_count} epochs {epoch_count}.h5'))
---
+++ src/latent_audio/scripts/evaluate_disentangle.py
@@ -0,0 +1,174 @@
+"""This script visualizes the model created by the disentangle script. It passes sample data through the model
+and shows in scatter plots how well the matierial and action factors are disentangled."""
+
+from latent_audio.scripts import disentangle as lsd
+from latent_audio.yamnet import layer_wise as lyl
+from typing import List, Any, OrderedDict, Callable, Generator, Tuple
+import os, numpy as np
+import tensorflow as tf, matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from gyoza.modelling import flow_layers as mfl
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
+import pickle as pkl
+
+# Define some functions
+def scatter_plot(flow_network, Z, Y, material_labels, action_labels, plot_save_path) -> None:
+
+    # Predict
+    Z_tilde = flow_network(Z)
+    Z_tilde_m = Z_tilde[:,-2] # Material dimension
+    Z_tilde_a = Z_tilde[:,-1] # Action dimension
+
+    # Plot
+    plt.subplots(2, 4, figsize=(12,6), gridspec_kw={'width_ratios': [1,5,1,5], 'height_ratios': [5,1]})
+
+    plt.suptitle(r"$\tilde{Z}$")
+
+    # 1. Materials
+    ms = list(set(Y[:,-2]))
+    
+    # 1.1 Vertical Boxplot
+    plt.subplot(2,4,1)
+    plt.boxplot([Z_tilde_a[Y[:,-2]==m] for m in ms])
+    plt.xticks(list(range(1,len(ms)+1)), [material_labels[m] for m in ms])
+    plt.ylabel("Action Dimension")
+    ax = plt.gca();ax.spines['top'].set_visible(False); ax.spines['right'].set_visible(False); ax.spines['bottom'].set_visible(False); ax.spines['left'].set_visible(False)
+    plt.yticks([])
+
+    # 1.2 Horizontal Boxplot
+    plt.subplot(2,4,6)
+    plt.boxplot([Z_tilde_m[Y[:,-2]==m] for m in ms], vert=False)
+    plt.yticks(list(range(1,len(ms)+1)), [material_labels[m] for m in ms])
+    plt.xlabel("Material Dimension")
+    ax = plt.gca();ax.spines['top'].set_visible(False); ax.spines['right'].set_visible(False); ax.spines['bottom'].set_visible(False); ax.spines['left'].set_visible(False)
+    plt.xticks([])
+
+    # 1.3 Scatter
+    plt.subplot(2,4,2); plt.title("Materials")
+    
+    for m in ms:
+        plt.scatter(Z_tilde_m[Y[:,-2]==m], Z_tilde_a[Y[:,-2]==m], marker='.')
+    plt.legend([material_labels[m] for m in ms])
+    
+    # 2. Action
+    As = list(set(Y[:,-1]))
+    
+    # 2.1 Vertical Boxplot
+    plt.subplot(2,4,3)
+    plt.boxplot([Z_tilde_a[Y[:,-1]==a] for a in As])
+    plt.xticks(list(range(1,len(As)+1)), [action_labels[a] for a in As])
+    plt.ylabel("Action Dimension")
+    ax = plt.gca();ax.spines['top'].set_visible(False); ax.spines['right'].set_visible(False); ax.spines['bottom'].set_visible(False); ax.spines['left'].set_visible(False)
+    plt.yticks([])
+
+    # 2.2 Horizontal Boxplot
+    plt.subplot(2,4,8)
+    plt.boxplot([Z_tilde_m[Y[:,-1]==a] for a in As], vert=False)
+    plt.yticks(list(range(1,len(As)+1)), [action_labels[a] for a in As])
+    plt.xlabel("Material Dimension")
+    ax = plt.gca();ax.spines['top'].set_visible(False); ax.spines['right'].set_visible(False); ax.spines['bottom'].set_visible(False); ax.spines['left'].set_visible(False)
+    plt.xticks([])
+
+    # 2.3 Scatter
+    plt.subplot(2,4,4); plt.title("Actions")
+    
+    for a in As:
+        plt.scatter(Z_tilde_m[Y[:,-1]==a], Z_tilde_a[Y[:,-1]==a], marker='.')
+    plt.legend([action_labels[a] for a in As])
+    
+    # Remove other axes
+    plt.subplot(2,4,5); plt.axis('off'); plt.subplot(2,4,7); plt.axis('off'); 
+    plt.savefig(os.path.join(plot_save_path, "Scatterplots.png"))
+    plt.show()
+
+def evaluate_factor_sensitivity(flow_network: mfl.SupervisedFactorNetwork, Z_tilde_to_semantic: Callable, iterator: Callable, sample_size: int, dimensions_per_factor: List[int], plot_save_path: str) -> None:
+
+    # Compute distance between pairs
+    delta_ma = compute_paired_differences(flow_network=flow_network, iterator=iterator, equal_material=True, equal_action=True, sample_size=sample_size) # ma means equal material, equal action
+    delta_m = compute_paired_differences(flow_network=flow_network, iterator=iterator, equal_material=True, equal_action=False, sample_size=sample_size) # m means equal material, different action
+    delta_a = compute_paired_differences(flow_network=flow_network, iterator=iterator, equal_material=False, equal_action=True, sample_size=sample_size) # a means different material, equal action
+
+    count = 0
+    distance = lambda x: tf.sqrt(tf.reduce_sum(x**2, axis=1)) # Shape of output == [instance count]
+    factor_names = ['Residual','Material','Action']
+    for f, dimension_count in enumerate(dimensions_per_factor):
+        # Plot
+        plt.figure(); plt.title(f'Sensitivity Of {factor_names[f]} Dimensions '+ r'Of $\tilde{Z}$' + '\nTo Changes In Z')
+        plt.violinplot([distance(delta_ma[:,count:count+dimension_count]), distance(delta_m[:,count:count+dimension_count]), distance(delta_a[:,count:count+dimension_count])], showmeans=True)
+        plt.xticks(ticks=[1,2,3], labels=['Same Material\nSame Action', 'Same Material\nOther Action', 'Other Material\nSame Action'])
+        plt.ylabel('Distance'); plt.xlabel("Input Pair")
+        count += dimension_count
+        plt.savefig(os.path.join(plot_save_path, f' Sensitivity {factor_names[f]}.png'))
+    plt.show()
+
+def compute_paired_differences(flow_network: mfl.SupervisedFactorNetwork, Z_tilde_to_semantic: Callable, iterator: Callable, equal_material: bool, equal_action: bool, sample_size: int) -> None:
+
+    # Initialization
+    Z_ab = []
+    current_sample_size = 0
+
+    # Accumulate instances to meet the sample size
+    while current_sample_size < sample_size:
+        Z_ab_batch, Y_ab_batch = next(iterator)
+        Z_ab.append(Z_ab_batch.numpy()[np.logical_and(Y_ab_batch[:,1] == equal_material, Y_ab_batch[:,2] == equal_action),:])
+        
+        current_sample_size += Z_ab[-1].shape[0]
+
+    # Obtain proper shape
+    Z_ab = np.concatenate(Z_ab, axis=0) # Instance axis
+    Z_ab = Z_ab[:sample_size,:] # Shape == [sample_size, 2, ...] where 2 is for pair and ... is for a single instance
+
+    # Distance vectors in disentangled space
+    delta = tf.reshape(flow_network(Z_ab[:,0,:]) - flow_network(Z_ab[:,1,:]), [sample_size, -1]) # Shape == [instance count, dimension count]
+
+    # Output
+    return delta
+
+# Configuration
+inspection_layer_index = 8
+data_path = os.path.join('data','pre-processed','64 PCA dimensions all in 1 file','Layer 8')
+batch_size = 512
+np.random.seed(850)
+stage_count = 10
+epoch_count = 150
+dimensions_per_factor = [14,1,1]
+materials_to_keep = [1,4]; actions_to_keep = [0,1]
+materials_to_drop = list(range(6))
+for m in reversed(materials_to_keep): materials_to_drop.remove(m)
+actions_to_drop = list(range(4))
+for a in reversed(actions_to_keep): actions_to_drop.remove(a)
+m_string = ",".join(str(m) for m in materials_to_keep)
+a_string = ",".join(str(a) for a in actions_to_keep)
+model_save_path = os.path.join('models', f'Layer {inspection_layer_index}')
+plot_save_path = os.path.join('plots','evaluate disentangle', f'Layer {inspection_layer_index}')
+if not os.path.exists(plot_save_path): os.makedirs(plot_save_path)
+material_labels = ['W','M','G','S','C','P']
+action_labels = ['T','R','D','W']
+
+# Load data iterators
+train_iterator, test_iterator, batch_count, Z_train, Z_test, Y_train, Y_test = lsd.load_iterators(data_path=data_path, materials_to_drop=materials_to_drop, actions_to_drop=actions_to_drop, batch_size=batch_size)
+Z_ab_sample, Y_ab_sample = next(train_iterator) # Sample
+
+print("The data is fed to the model in batches of shape:\n","Z: (instance count, pair, dimensionality): \t", Z_ab_sample.shape,'\nY_sample: (instance count, factor count): \t', Y_ab_sample.shape)
+
+# Create network
+flow_network = lsd.create_network(Z_sample=Z_ab_sample[:,0,:], stage_count=stage_count, dimensions_per_factor=dimensions_per_factor)
+flow_network.load_weights(os.path.join(model_save_path, f'materials {m_string} actions {a_string} stages {stage_count} epochs {epoch_count}.h5'))
+
+# Evaluate
+#scatter_plot(flow_network=flow_network, Z=Z_test, Y=Y_test, material_labels=material_labels, action_labels=action_labels, plot_save_path=plot_save_path)
+
+with open(os.path.join(model_save_path, 'Standard Scaler.pkl'), 'rb') as file_handle:
+    scaler = pkl.load(file_handle)
+with open(os.path.join(model_save_path, f'PCA {np.sum(dimensions_per_factor)}.pkl', 'rb')) as file_handle:
+    pca = pkl.load(file_handle)
+'''
+yamnet_second_half = lyl.LayerWiseYamnet()
+# Need the standard scaler from all dims
+# Need the pca from all dims to current dims
+# Need to invert both transforms
+Z_tilde_to_semantic = lambda x: yamnet_second_half.call_from_layer(latent: x, layer_index=inspection_layer_index)
+evaluate_factor_sensitivity(flow_network=flow_network, latent_to_semantic=latent_to_semantic, iterator=test_iterator, sample_size=500, dimensions_per_factor=dimensions_per_factor, plot_save_path=plot_save_path)
+'''
+k=3
