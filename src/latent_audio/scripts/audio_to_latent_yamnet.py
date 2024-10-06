7122e401b34a55c9682315053352aa88173fc050
TimHenry1995 <52284098+TimHenry1995@users.noreply.github.com>
2024-10-06 21:00:06 +0200

    Update Docs of audio_to_latent_yamnet.py

--- src/latent_audio/scripts/audio_to_latent_yamnet.py
+++ src/latent_audio/scripts/audio_to_latent_yamnet.py
@@ -3,37 +3,30 @@ import tensorflow as tf
 from latent_audio.yamnet import layer_wise as ylw
 import os, soundfile as sf, numpy as np, shutil
 from scipy.signal import decimate
+from typing import Dict, List
 
 def run(
     layer_index: int,
+    waveform_file_name_to_Y_vector: Dict[str, List[int]],
     raw_folder_path: str = os.path.join('data','raw audio'),
     augmented_folder_path: str = os.path.join('data','augmented audio'),
     latent_data_path: str = os.path.join('data','latent yamnet','original')
-    ):
-    """This function can be used to convert the raw audio data from their waveform representation into their latent yamnet representations.
+    ) -> None:
+    """This function loads each waveform file, converts it into a sequence of ca. 1 second long spectrogram slices and passes them through Yamnet up until `layer_index`.
+    Each slice will then be flattened into a long vector and saved to disk with the name \<waveform file name>\_X\_\<slice index>.npy next to a small file called \<waveform file name>\_Y\_\<slice index>.npy storing the vector with the factor-wise class indices of that slice.
+    For example, if the waveform file was called 'Metal Tapping.wav', the X files would be called 'Metal Tapping_X_1.npy', 'Metal Tapping_X_2.npy', etc. and the Y files 'Metal Tapping_Y_1.npy', 'Metal Tapping_Y_2.npy' etc.
+    Note that this function initially deletes the output folder (if exists) and then saves only the new data in it. Hence, any old data in that folder will get lost.
+    Apart from that, this function cleares the current Keras session and hence any variables stored in it will get lost.
 
-    Requirements:
-    - The script augment_audio needs to be executed apriori
-    - The raw and augmented audio data needs to be sampled at 48 Khz with int16 bit rate
-
-    Steps:
-    - iterates layer indices
-    - loads raw and augmented waveforms (will be concatenated and normalized to float32 in range [-1,1])
-    - passes through yamnet up until current layer
-    - flattens each time frame into a vector
-    - saves each time frame
-
-    Side effects:
-    - The script initially deletes the output folder (if exists) and the saves only the new data in it. Hence, any old data in that fodler will be lost.
-    - Keras session is cleared at beginning of call.
-        
     :param layer_index: The index of the Yamnet layer for which the latent Yamnet data shall be computed.
     :type layer_index: int
-    :param raw_folder_path: The path to the folder containing the raw, i.e. waveform data before augmentation.
+    :param waveform_file_name_to_Y_vector: A dictionary mapping the wavefomr file names (without their file extension) to Y vectors that will then be stored for each sound slice.
+    :type waveform_file_name_to_Y_vector: Dict[str, int]
+    :param raw_folder_path: The path to the folder containing the raw, i.e. waveform data before augmentation. This audio data needs to be sampled at 48 Khz with int16 bit rate.
     :type raw_folder_path: str
-    :param augmented_folder_path: The path to the folder containing the augmented, i.e. waveform data that augments the raw data.
+    :param augmented_folder_path: The path to the folder containing the augmented, i.e. waveform data that augments the raw data. Same sampling rate and bitrate is assumed. You can set this to None if you do not intend to use augmented data.
     :type augmented_folder_path: str
-    :param latent_data_path: The path to the folder in which the latent representations shall be stored.
+    :param latent_data_path: The path to the folder in which the latent representations shall be stored. The result will be a file called X.npy which stores a matrix of shape [instance count, dimensionality of latent yamnet at `layer_index`] and a file Y.npy of shape [instance count, 2] storing the material and action class, respectively, for every sound.
     :type latent_data_path: str
     """
 
@@ -50,10 +43,6 @@ def run(
     yamnet = ylw.LayerWiseYamnet()
     yamnet.load_weights(os.path.join('src','latent_audio','plugins','yamnet','yamnet.h5'))
 
-    material_to_label = {'W':0,'M':1,'G':2,'S':3,'C':4,'P':5}
-    action_to_label = {'T':0,'R':1,'D':2,'W':3}
-
-
     layer_path = os.path.join(latent_data_path,f'Layer {layer_index}')
 
     # This line deletes any content in the output folder 
@@ -63,19 +52,21 @@ def run(
     # Preprocess all files
     for r, raw_file_name in enumerate(raw_file_names):
         # Load .wav file
-        waveform_r, sampling_rate_r = sf.read(os.path.join(raw_folder_path, raw_file_name), dtype=np.int16)
-        assert sampling_rate_r == 48000, f"The sampling rate of the raw audio was assumed to be 48000 in order to apply the decimation algorithm and achieve yamnets 16000. The provided audio has sampling rate {sampling_rate_r}. You need to use a different downsampling method, e.g. from sklearn to meet yamnet's requirement."
-        waveform_a, sampling_rate_a = sf.read(os.path.join(augmented_folder_path, raw_file_name), dtype=np.int16)
-        assert sampling_rate_a == 48000, f"The sampling rate of the augmented audio was assumed to be 48000 in order to apply the decimation algorithm and achieve yamnets 16000. The provided audio has sampling rate {sampling_rate_a}. You need to use a different downsampling method, e.g. from sklearn to meet yamnet's requirement."
+        waveform, sampling_rate = sf.read(os.path.join(raw_folder_path, raw_file_name), dtype=np.int16)
+        assert sampling_rate == 48000, f"The sampling rate of the raw audio was assumed to be 48000 in order to apply the decimation algorithm and achieve yamnets 16000. The provided audio has sampling rate {sampling_rate_r}. You need to use a different downsampling method, e.g. from sklearn to meet yamnet's requirement."
+        if augmented_folder_path is not None:
+            waveform_a, sampling_rate_a = sf.read(os.path.join(augmented_folder_path, raw_file_name), dtype=np.int16)
+            assert sampling_rate_a == 48000, f"The sampling rate of the augmented audio was assumed to be 48000 in order to apply the decimation algorithm and achieve yamnets 16000. The provided audio has sampling rate {sampling_rate_a}. You need to use a different downsampling method, e.g. from sklearn to meet yamnet's requirement."
         
         # Normalize
-        waveform_r =  waveform_r.astype('float32')
-        waveform_r = waveform_r / np.max(waveform_r)
-        waveform_a = waveform_a.astype('float32')
-        waveform_a = waveform_a / np.max(waveform_a)
+        waveform =  waveform.astype('float32')
+        waveform = waveform / np.max(waveform)
+        if augmented_folder_path is not None:
+            waveform_a = waveform_a.astype('float32')
+            waveform_a = waveform_a / np.max(waveform_a)
 
-        # Concatenate raw and augmented
-        waveform = np.concatenate([waveform_r, waveform_a])
+            # Concatenate raw and augmented
+            waveform = np.concatenate([waveform, waveform_a])
         
         # Down-sample to yamnet's 16000
         waveform = decimate(waveform, 3)
@@ -85,20 +76,16 @@ def run(
         latent = yamnet.call_until_layer(waveform=waveform, layer_index=layer_index).numpy()
 
         # Create y
-        material = raw_file_name[0]; action = raw_file_name[1]
-        y = np.array([material_to_label[material], action_to_label[action]])
+        name = raw_file_name.split('.')[:-1].join('.') # removes the file extension
+        y = np.array(waveform_file_name_to_Y_vector[name])
 
         # Save
         for s, slice in enumerate(latent):
-            np.save(os.path.join(layer_path, f"{raw_file_name[:-4]}_X_{s}.npy"), np.reshape(slice,[-1])) 
-            np.save(os.path.join(layer_path, f"{raw_file_name[:-4]}_Y_{s}.npy"), y) 
+            np.save(os.path.join(layer_path, f"{name}_X_{s}.npy"), np.reshape(slice,[-1])) 
+            np.save(os.path.join(layer_path, f"{name}_Y_{s}.npy"), y) 
         print(f"\r\t{np.round(100*(r+1)/len(raw_file_names))}% Completed", end='')
 
         # Delete singleton
         del latent
 
     print("\n\tRun Completed")
-
-if __name__ == "__main__":
-    for layer_index in range(14):
-        run(layer_index=layer_index)       
