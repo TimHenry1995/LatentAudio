9ce4c5be9a4e00fbf04da5224c654aa78d542afe
TimHenry1995 <52284098+TimHenry1995@users.noreply.github.com>
2024-10-07 21:36:43 +0200

    Update docs for  latent_yamnet_to_calibration_data_set.py

--- src/latent_audio/scripts/latent_yamnet_to_calibration_data_set.py
+++ src/latent_audio/scripts/latent_yamnet_to_calibration_data_set.py
@@ -1,61 +1,66 @@
-"""This takes the individual files from the audio_to_latent_yamnet script, projects them to a manageable dimensionality and
-combines them into a single file for calibration of the flow network. It takes long as is not meant to be executed for all layers.
+"""This takes the individual files of latent Yamnet sound representations, projects them to a manageable dimensionality and
+combines them into a single file for calibration of the flow network. Note, the output files (if exist) will be deleted before the new files are created.
 
-Requirements:
-- the audio_to_latent_yamnet script needs to be executed apriori
-- the create_scaler_and_PCA_model_for_latent_yamnet script needs to be executed a priori
 
-Steps:
-- for a given layer it loads each data point
-- passes it through the pre PCA scaler, the complete PCA and the post PCA scaler
-- takes the top k dimensions, where k is a number mangeable by the downstream models
-- collects all data points in one file
-- saves them all in one file called X.npy along with the labels in Y.npy
-
-Side effects:
-- The output files (if exist) will be deleted before the new files are created.
+:param layer_index: The index of the layer for which the data shall be converted.
+:type layer_index: int
+:param dimensionality: The number of dimensions that the projection shall have. Note that this number must not be greater than what the PCA model saved at `pca_path` supports.
+:type dimensionality: int 
+:param input_data_path: The data to the input data. These are assumed to be stored in the same format as done by `audio_to_latent_yamnet.run()`.
+:type input_data_path: str
+:param pca_path: The path to the standard scalers and PCA model. Models are assumed to be stored in the same format as done by `create_scalers_and_PCA_mode_for_latent_yamnet`.
+:type pca_path: str
+:param output_data_path: The path to the folder where the output data shall be stored. The output will be stored in a folder called 'Layer `layer_index`' as X.npy of shape [instance count, `dimensionality`] and corresponding Y.npy of shape [instance count, factor count].
 """
 
 from latent_audio import utilities as utl
 import os, pickle as pkl, numpy as np, shutil
 
-# Configuration
-dimensionality = 64
-input_data_path = os.path.join('data','latent yamnet','original')
-pca_path = os.path.join('models','Scaler and PCA')
-output_data_path = os.path.join('data','latent yamnet', f"{dimensionality} dimensions")
-layer_index = 9
+def run(layer_index: int,
+        dimensionality: int = 64, 
+        input_data_path: str = os.path.join('data','latent yamnet','original'), 
+        pca_path : str = os.path.join('models','Scaler and PCA'), 
+        output_data_path: str = os.path.join('data','latent yamnet')):
 
-# Processing
-input_layer_path = os.path.join(input_data_path, f'Layer {layer_index}')
-output_layer_path = os.path.join(output_data_path, f'Layer {layer_index}')
-pca_layer_path = os.path.join(pca_path, f"Layer {layer_index}")
-if os.path.exists(output_layer_path): shutil.rmtree(pca_layer_path)
-os.makedirs(output_layer_path)
+    print("Running script to convert latent yamnet to calibration data set.")
 
-# Loads models
-with open(os.path.join(pca_layer_path, "Pre PCA Standard Scaler.pkl"),'rb') as fh:
-    pre_scaler = pkl.load(fh)
+    # Adjust configuration
+    output_data_path = os.path.join(output_data_path, f"{dimensionality} dimensions")
 
-with open(os.path.join(pca_layer_path, "Complete PCA.pkl"),'rb') as fh:
-    pca = pkl.load(fh)
+    # Processing
+    input_layer_path = os.path.join(input_data_path, f'Layer {layer_index}')
+    output_layer_path = os.path.join(output_data_path, f'Layer {layer_index}')
+    pca_layer_path = os.path.join(pca_path, f"Layer {layer_index}")
+    if os.path.exists(output_layer_path): shutil.rmtree(output_layer_path)
+    os.makedirs(output_layer_path)
 
-with open(os.path.join(pca_layer_path, "Post PCA Standard Scaler.pkl"), 'rb') as fh:
-    post_scaler = pkl.load(fh)
+    # Loads models
+    with open(os.path.join(pca_layer_path, "Pre PCA Standard Scaler.pkl"),'rb') as fh:
+        pre_scaler = pkl.load(fh)
 
-print(f"The top {dimensionality} dimensions explain {np.round(100 * np.sum(pca.explained_variance_ratio_[:dimensionality]),2)} % of variance.")
+    with open(os.path.join(pca_layer_path, "Complete PCA.pkl"),'rb') as fh:
+        pca = pkl.load(fh)
 
-# Transform each X
-x_file_names = utl.find_matching_strings(strings=os.listdir(input_layer_path), token='_X_')
-Xs = [None] * len(x_file_names); Ys = [None] * len(x_file_names)
-for i, x_file_name in enumerate(x_file_names):
-    X = np.load(os.path.join(input_layer_path, x_file_name))[np.newaxis,:]
-    Xs[i] = post_scaler.transform(pca.transform(pre_scaler.transform(X)))[:,:dimensionality] 
-    Ys[i] = np.load(os.path.join(input_layer_path, x_file_name.replace('_X_','_Y_')))[np.newaxis,:]
-    if (i+1) % (int)(len(x_file_names)/10) == 0:
-        print(f"{np.round(100*(i+1)/len(x_file_names),2)} % Finished")
-# Save
-np.save(os.path.join(output_layer_path, "X"), np.concatenate(Xs, axis=0))
-np.save(os.path.join(output_layer_path, "Y"), np.concatenate(Ys, axis=0))
-    
-print("Script completed")
+    with open(os.path.join(pca_layer_path, "Post PCA Standard Scaler.pkl"), 'rb') as fh:
+        post_scaler = pkl.load(fh)
+
+    print(f"\tThe top {dimensionality} dimensions explain {np.round(100 * np.sum(pca.explained_variance_ratio_[:dimensionality]),2)} % of variance.")
+
+    # Transform each X
+    x_file_names = utl.find_matching_strings(strings=os.listdir(input_layer_path), token='_X_')
+    Xs = [None] * len(x_file_names); Ys = [None] * len(x_file_names)
+    for i, x_file_name in enumerate(x_file_names):
+        X = np.load(os.path.join(input_layer_path, x_file_name))[np.newaxis,:]
+        Xs[i] = post_scaler.transform(pca.transform(pre_scaler.transform(X)))[:,:dimensionality] 
+        Ys[i] = np.load(os.path.join(input_layer_path, x_file_name.replace('_X_','_Y_')))[np.newaxis,:]
+      
+        print(f"\r\t{np.round(100*(i+1)/len(x_file_names))} % Completed", end='')
+    # Save
+    np.save(os.path.join(output_layer_path, "X"), np.concatenate(Xs, axis=0))
+    np.save(os.path.join(output_layer_path, "Y"), np.concatenate(Ys, axis=0))
+        
+    print("\n\tRun completed")
+
+if __name__ == "__main__":
+    for layer_index in range(14):
+        run(layer_index=layer_index)
